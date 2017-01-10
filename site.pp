fc2a192f2789add0980b112a809533177b36c5c3
pavan-koundinya <pavan@kloudxp.com>
2017-01-10 10:22:06 -0800

    ucsm-puppet Type/provider implementation

---
+++ site.pp
@@ -0,0 +1,12 @@
+boot_policy{'policy':
+	confuse => 'testing123',
+	descr => 'type  description',
+	reboot_on_update => 'no',
+	policy_owner => 'local',
+	enforce_vnic_name => 'yes',
+	boot_mode => 'uefi',
+	state => 'absent',	
+        ip => '172.31.219.215',
+        username => 'ucspe',
+        password => 'ucspe',
+}
